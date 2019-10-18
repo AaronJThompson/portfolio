02a0de02aaa57c8be49e3dc4de544b116f44f4d4
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-19 00:09:30 +0100

    Fix 404 page an make responsive

--- gatsby-site/src/pages/404.js
+++ gatsby-site/src/pages/404.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Helmet from 'react-helmet'
+
 import Layout from '../components/layout'
+import Header from '../components/Header'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Helmet title="Page Not Found" />
+    <Header className="error">
+      <h1>404</h1>
+    </Header>
+    <div id='main'>
+      <section className="main">
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </section>
+    </div>
   </Layout>
 )
 
