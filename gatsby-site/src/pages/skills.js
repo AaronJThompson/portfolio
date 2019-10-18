baec565239d43a44a45d62e8629d1d435a368bf7
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-18 22:14:35 +0100

    Add skill colors'

--- gatsby-site/src/pages/skills.js
+++ gatsby-site/src/pages/skills.js
@@ -24,27 +24,27 @@ class Generic extends React.Component {
             </header>
             <ul className="languages">
               <li>
-                <span className="icon medium style5 fab fa-js-square"></span>
+                <span className="icon medium style-js fab fa-js-square"></span>
                 <h3>Javascript</h3>
               </li>
               <li>
-                <span className="icon medium style2 fab fa-html5"></span>
+                <span className="icon medium style-html fab fa-html5"></span>
                 <h3>HTML</h3>
               </li>
               <li>
-                <span className="icon medium style3 fab fa-css3"></span>
+                <span className="icon medium style-css fab fa-css3"></span>
                 <h3>CSS</h3>
               </li>
               <li>
-                <span className="icon medium fas fa-database"></span>
+                <span className="icon medium style-sql fas fa-database"></span>
                 <h3>SQL</h3>
               </li>
               <li>
-                <span className="icon medium fab fa-python"></span>
+                <span className="icon medium style-python fab fa-python"></span>
                 <h3>Python</h3>
               </li>
               <li>
-                <span className="icon medium fas fa-code"></span>
+                <span className="icon medium style-lua fas fa-moon"></span>
                 <h3>Lua/LuaJIT</h3>
               </li>
             </ul>
@@ -55,15 +55,15 @@ class Generic extends React.Component {
             </header>
             <ul className="languages">
               <li>
-                <span className="icon medium style5 fab fa-react"></span>
+                <span className="icon medium style-react fab fa-react"></span>
                 <h3>ReactJS</h3>
               </li>
               <li>
-                <span className="icon medium style5 fas fa-code"></span>
+                <span className="icon medium style-redux fas fa-box-open"></span>
                 <h3>ReduxJS</h3>
               </li>
               <li>
-                <span className="icon medium style5 fab fa-node-js"></span>
+                <span className="icon medium style-node fab fa-node-js"></span>
                 <h3>NodeJS</h3>
               </li>
               <li>
@@ -78,23 +78,27 @@ class Generic extends React.Component {
             </header>
             <ul className="languages">
               <li>
-                <span className="icon medium style5 fab fa-node-js"></span>
+                <span className="icon medium style1 fas fa-server"></span>
                 <h3>REST API's</h3>
               </li>
               <li>
-                <span className="icon medium style5 fas fa-server"></span>
+                <span className="icon medium style1 fas fa-user-lock"></span>
                 <h3>Authentication</h3>
               </li>
               <li>
-                <span className="icon medium style5 fas fa-server"></span>
+                <span className="icon medium style2 fas fa-pencil-ruler"></span>
+                <h3>Web Design</h3>
+              </li>
+              <li>
+                <span className="icon medium style3 fas fa-file-code"></span>
                 <h3>Single Page Applications</h3>
               </li>
               <li>
-                <span className="icon medium style5 fab fa-react"></span>
+                <span className="icon medium style4 fas fa-vial"></span>
                 <h3>Test Driven Development</h3>
               </li>
               <li>
-                <span className="icon medium style5 fas fa-code"></span>
+                <span className="icon medium style5 fas fa-undo-alt"></span>
                 <h3>Agile development</h3>
               </li>
             </ul>
