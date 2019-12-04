028f163bd0faedb906f9cba2100d18355aaaf1a5
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-04 16:15:24 +0000

    Add imports from projects page

---
+++ gatsby-site/src/pages/projects.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import { Link } from 'gatsby'
+
+import NavBar from '../components/NavBar'
+import Layout from '../components/layout'
+import Header from '../components/Header'
