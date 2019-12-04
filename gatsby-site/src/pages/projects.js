6e2813f6c534c4b462ea8f3ed5162ada5ce94f4f
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-04 16:20:42 +0000

    Add basic page structure

--- gatsby-site/src/pages/projects.js
+++ gatsby-site/src/pages/projects.js
@@ -5,3 +5,19 @@ import { Link } from 'gatsby'
 import NavBar from '../components/NavBar'
 import Layout from '../components/layout'
 import Header from '../components/Header'
+
+export default function (props) {
+  return(
+    <Layout>
+      <Helmet title="Aaron Thompson - Projects" />
+      <Header>
+        <h1>My Projects</h1>
+        <p>The projects I have completed and the story behind them</p>
+      </Header>
+      <Link to="/" className="button home">
+        Back Home
+      </Link>
+      <NavBar items={['projects']} names={['Projects']} />
+    </Layout>
+  )
+}
