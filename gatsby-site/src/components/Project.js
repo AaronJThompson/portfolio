858822bab0c23db003250c995c9323c06910f27a
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-05 14:48:44 +0000

    Add header to Project component

--- gatsby-site/src/components/Project.js
+++ gatsby-site/src/components/Project.js
@@ -2,4 +2,13 @@ import React from 'react';
 
 export default function(props) {
   const { img_src, title, git_link, deploy_link, short_desc, long_desc} = props;
+
+  return (
+    <div className="project">
+      <header>
+        <div className="image" style={{backgroundImage: "url(" + img_src + ")"}} />
+        <h2>{title}</h2>
+      </header>
+    </div>
+  )
 }
