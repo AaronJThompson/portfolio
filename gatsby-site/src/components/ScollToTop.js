891111add95dcb8586d31589a533601567812a09
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-09 18:12:57 +0000

    Add waypoint for visibility trigger

--- gatsby-site/src/components/ScollToTop.js
+++ gatsby-site/src/components/ScollToTop.js
@@ -1,11 +1,26 @@
 import React, { useState } from 'react'
+import { Waypoint } from 'react-waypoint'
 import Scroll from './Scroll'
 
 const ScrollToTop = (props) => {
+  const [visible, setVisible] = useState(false);
+
+  const handleWaypointEnter = () => {
+    setVisible(true)
+  }
+  const handleWaypointLeave = () => {
+    setVisible(false)
+  }
   return (
-    <Scroll type="id" element="header">
-      <a href="#" className="button scroll-top">Scroll to top</a>
-    </Scroll>
+    <>
+      <Waypoint
+        onEnter={handleWaypointEnter}
+        onLeave={handleWaypointLeave}
+      ></Waypoint>
+      <Scroll type="id" element="header">
+        <a href="#" className={`button scroll-top${visible ? "" : " visible"}`}>Scroll to top</a>
+      </Scroll>
+    </>
   )
 }
 
