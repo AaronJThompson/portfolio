e0cb2ef4ed8e184c72ff84fea7c0601f0ab3b08b
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-09 18:07:47 +0000

    Add basic JSX for scroll to top button

--- gatsby-site/src/components/ScollToTop.js
+++ gatsby-site/src/components/ScollToTop.js
@@ -2,5 +2,9 @@ import React, { useState } from 'react'
 import Scroll from './Scroll'
 
 const ScrollToTop = (props) => {
-  
+  return (
+    <Scroll type="id" element="header">
+      <a href="#" className="button scroll-top">Scroll to top</a>
+    </Scroll>
+  )
 }
