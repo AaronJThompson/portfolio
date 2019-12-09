e67c3adac3a59100811c858aa0da199152321765
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-09 21:37:30 +0000

    Hide page scroll when modal is active

--- gatsby-site/src/components/Modal.js
+++ gatsby-site/src/components/Modal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Modal = (props) => {
   let { active, children, bgClick } = props;
@@ -10,6 +10,15 @@ const Modal = (props) => {
   const stopBubble = (e) => {
     e.stopPropagation();
   }
+
+  useEffect(() => {
+    if (active) {
+      document.body.style.overflowY = "hidden";
+    } else {
+      document.body.style.overflowY = "initial";
+    }
+  }, [active])
+
 
   return (
     <section onClick={bgClick} className={`modal${active ? " active" : ""}`}>
