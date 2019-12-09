acd93c407d952166d3a08f893c5144849f003d02
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-09 17:09:04 +0000

    Add sleep tracker product

--- gatsby-site/src/pages/projects.js
+++ gatsby-site/src/pages/projects.js
@@ -25,8 +25,9 @@ export default function (props) {
               <h2>Projects</h2>
             </header>
             <ul className="projects">
-              <Project img_src="https://imgur.com/FVK9QmN.png" title="Bravo" git_link="https://github.com/bravolabs" deploy_link="https://saybravo.io/" short_desc="Bravo is an employee recognition and incentive platform aimed at small to medium-sized teams built in 8 weeks with 8 developers" long_dec="Bravo is an employee recognition and incentive platform aimed at small to medium-sized teams built in 8 weeks with 8 developers"/>
-              <Project img_src="https://imgur.com/C6uYsJN.png" title="Water My Plants" git_link="https://github.com/bw-water-my-plants" deploy_link="https://water-my-plants-554a4.firebaseapp.com/" short_desc="Water My Plants is a plant watering reminder and scheduler built by a team of 3 at Lambda School in 1 week" long_dec="Water My Plants is a plant watering reminder and scheduler built by a team of 3 at Lambda School in 1 week"/>
+              <Project img_src="https://imgur.com/FVK9QmN.png" title="Bravo" git_link="https://github.com/bravolabs" deploy_link="https://saybravo.io/" short_desc="Bravo is an employee recognition and incentive platform aimed at small to medium-sized teams built in 8 weeks with 8 developers"/>
+              <Project img_src="https://imgur.com/C6uYsJN.png" title="Water My Plants" git_link="https://github.com/bw-water-my-plants" deploy_link="https://water-my-plants-554a4.firebaseapp.com/" short_desc="Water My Plants is a plant watering reminder and scheduler built by a team of 3 at Lambda School in 1 week"/>
+              <Project img_src="https://imgur.com/NjLjiJS.png" title="Sleep Tracker" git_link="https://github.com/build-week-sleep-tracker" deploy_link="https://bw-sleep-tracker-fe.netlify.com/" short_desc="Sleep Tracker is an app enabling users to track their sleep and receive feedback and reccomendations on their sleep patterns. This was built by 6 developers in 1 week" a/>
             </ul>
         </section>
       </div>
