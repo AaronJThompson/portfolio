8588d1323680933c8c60da3d87f4481ec4c4f6e4
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-09 17:34:16 +0000

    Move project list to json

--- gatsby-site/src/pages/projects.js
+++ gatsby-site/src/pages/projects.js
@@ -6,9 +6,10 @@ import NavBar from '../components/NavBar'
 import Layout from '../components/layout'
 import Header from '../components/Header'
 import Project from '../components/Project'
+import _projectList from '../content/projects.json';
 
 export default function (props) {
-  return(
+  return (
     <Layout>
       <Helmet title="Aaron Thompson - Projects" />
       <Header>
@@ -21,15 +22,14 @@ export default function (props) {
       <NavBar items={['projects']} names={['Projects']} />
       <div id="main">
         <section id="intro" className="main special">
-            <header className="major">
-              <h2>Projects</h2>
-            </header>
-            <ul className="projects">
-              <Project img_src="https://imgur.com/FVK9QmN.png" title="Bravo" git_link="https://github.com/bravolabs" deploy_link="https://saybravo.io/" short_desc="Bravo is an employee recognition and incentive platform aimed at small to medium-sized teams built in 8 weeks with 8 developers"/>
-              <Project img_src="https://imgur.com/C6uYsJN.png" title="Water My Plants" git_link="https://github.com/bw-water-my-plants" deploy_link="https://water-my-plants-554a4.firebaseapp.com/" short_desc="Water My Plants is a plant watering reminder and scheduler built by a team of 3 at Lambda School in 1 week"/>
-              <Project img_src="https://imgur.com/NjLjiJS.png" title="Sleep Tracker" git_link="https://github.com/build-week-sleep-tracker" deploy_link="https://bw-sleep-tracker-fe.netlify.com/" short_desc="Sleep Tracker is an app enabling users to track their sleep and receive feedback and reccomendations on their sleep patterns. This was built by 6 developers in 1 week" a/>
-              <Project img_src="https://imgur.com/qwJuxrO.png" title="[ARRAY]" git_link="https://github.com/arrayhackathon2019" short_desc="Array is a social media platform for software engineers and coders alike. This was a concept project built in 48 hours during the Lambda Hackathon with a team of 5 developers" a/>
-            </ul>
+          <header className="major">
+            <h2>Projects</h2>
+          </header>
+          <ul className="projects">
+            {_projectList.map(proj => {
+              return <Project {...proj} />
+            })}
+          </ul>
         </section>
       </div>
     </Layout>
